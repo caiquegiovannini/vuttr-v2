f9176656acab394145db40d638585b3bf41a1d14
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2023-06-28 23:59:23 -0300

    Create home container

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,8 @@
+import './styles.css'
+
 export function Home() {
     return (
-        <section>
+        <section className='container'>
             <h1>VUTTR</h1>
             <h2>Very Useful Tools To Remember</h2>
         </section>
