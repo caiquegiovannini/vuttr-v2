d3c203642691fa7d4d7b3ebe6b7ee08b37f0a9c8
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2023-07-03 22:41:58 -0300

    Add remove button

--- src/components/tool-card/index.tsx
+++ src/components/tool-card/index.tsx
@@ -1,27 +1,33 @@
+import TrashIcon from '../../assets/trash.svg'
+
 import './styles.css'
 
 interface ToolCardProps {
     title: string
+    url: string
     description: string
     tags: string[]  // cada tag precisa ser única
 }
 
-export function ToolCard({title, description, tags}: ToolCardProps) {
+export function ToolCard({ title, url, description, tags }: ToolCardProps) {
     return (
         <article className='tool-card'>
             <header className='tool-card__header'>
                 <h3>
-                    <a href="#">{title}</a>
+                    <a
+                        href={`https://${url}`}
+                        target='_blank'
+                    >{title}</a>
                 </h3>
-                <a href="#">
-                    <h3>{title}</h3>
-                </a>
+                <button className='tool-card__header__remove-button'>
+                    <img src={TrashIcon} alt="trash can" />
+                </button>
             </header>
             <p className='tool-card__description'>{description}</p>
             <footer className='tool-card__footer'>
                 {tags.map(tag => (
                     <span key={tag} className='tool-card__footer__tag'>#{tag}</span>
-                ))}                
+                ))}
             </footer>
         </article>
     )
