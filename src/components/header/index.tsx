b653cc0bfbbd078965474eb7a1198dd47255faf9
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2023-07-06 21:44:45 -0300

    Create Header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export function Header() {
+    return (
+        <header className='header'>
+            <h1>VUTTR</h1>
+            <h2>Very Useful Tools To Remember</h2>
+        </header>
+    )
+}
