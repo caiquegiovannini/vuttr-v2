f6d0ed3262d4da115fa2680571adb998fb369a2d
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2023-07-11 00:07:31 -0300

    Add new tool modal

--- src/components/actions/index.tsx
+++ src/components/actions/index.tsx
@@ -1,16 +1,42 @@
+import * as Dialog from '@radix-ui/react-dialog';
 import Add from '../../assets/add.svg'
 
 import './styles.css'
+import { Input } from '../input';
 
 export function Actions() {
     return (
-        <aside className='actions-container'>
-            <button
-                className='add-button'
-                aria-label='Add new tool button'
-            >
-                <img src={Add} alt='' />
-            </button>
-        </aside>
+        <Dialog.Root>
+            <aside className='actions-container'>
+                <Dialog.Trigger asChild>
+                    <button
+                        className='add-button'
+                        aria-label='Add new tool button'
+                    >
+                        <img src={Add} alt='' />
+                    </button>
+                </Dialog.Trigger>
+            </aside>
+            <Dialog.Portal>
+                <Dialog.Overlay className='modal__overlay' />
+                <Dialog.Content className='modal'>
+                    <Dialog.Title className='modal__title'>
+                        Add new tool
+                    </Dialog.Title>
+                    <form className='modal__form'>
+                        <Input id='tool-name' label='Tool name' />
+                        <Input id='tool-link' label='Tool link' prefix='https://' />
+                    </form>
+                    <Dialog.Close asChild>
+                        <button
+                            className='modal__close-button'
+                            aria-label='Close button'
+                        >
+                            <img src={Add} alt='' />
+                        </button>
+                    </Dialog.Close>
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
     )
 }
