c6eb4809da3e132d44b8f240d1686d2f2116c735
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2023-07-23 01:01:48 -0300

    Remove radix wrapper from submit button

--- src/components/actions/new-tool-modal/index.tsx
+++ src/components/actions/new-tool-modal/index.tsx
@@ -14,37 +14,33 @@ export function NewToolModal() {
     const tagsRef = useRef<HTMLInputElement>(null)
 
     return (
-            <Dialog.Portal>
-                <Dialog.Overlay className='modal__overlay' />
-                <Dialog.Content className='modal'>
-                    <Dialog.Title className='modal__title'>
-                        Add new tool
-                    </Dialog.Title>
-                    <form className='modal__form'>
-                        <Input id='tool-name' label='Tool name' inputRef={toolNameRef} />
-                        <Input id='tool-link' label='Tool link' prefix='https://' inputRef={toolLinkRef} />
-                        <Textarea id='tool-description' label='Tool description'  textareaRef={toolDescriptionRef}>
-                            nada
-                        </Textarea>
-                        <Input id='tool-tags' label='Tags' placeholder='separate tags by space' inputRef={tagsRef} />
-                        <Dialog.Close asChild>
-                            <button 
-                                type='submit' 
-                                className='modal__add-tool-button'
-                            >
-                                Add tool
-                            </button>
-                        </Dialog.Close>
-                    </form>
-                    <Dialog.Close asChild>
-                        <button
-                            className='modal__close-button'
-                            aria-label='Close button'
-                        >
-                            <img src={Add} alt='' />
-                        </button>
-                    </Dialog.Close>
-                </Dialog.Content>
-            </Dialog.Portal>
+        <Dialog.Portal>
+            <Dialog.Overlay className='modal__overlay' />
+            <Dialog.Content className='modal'>
+                <Dialog.Title className='modal__title'>
+                    Add new tool
+                </Dialog.Title>
+                <form className='modal__form' onSubmit={onSubmit}>
+                    <Input id='tool-name' label='Tool name' inputRef={toolNameRef} />
+                    <Input id='tool-link' label='Tool link' prefix='https://' inputRef={toolLinkRef} />
+                    <Textarea id='tool-description' label='Tool description' textareaRef={toolDescriptionRef} />
+                    <Input id='tool-tags' label='Tags' placeholder='separate tags by space' inputRef={tagsRef} />
+                    <button
+                        type='submit'
+                        className='modal__add-tool-button'
+                    >
+                        Add tool
+                    </button>
+                </form>
+                <Dialog.Close asChild>
+                    <button
+                        className='modal__close-button'
+                        aria-label='Close button'
+                    >
+                        <img src={Add} alt='' />
+                    </button>
+                </Dialog.Close>
+            </Dialog.Content>
+        </Dialog.Portal>
     )
 }
