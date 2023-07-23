4711500d1bc657964aa0005ff92ea3119365909a
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2023-07-22 22:48:10 -0300

    Type fetch tools return

--- src/api/tools.ts
+++ src/api/tools.ts
@@ -1,3 +1,4 @@
 import { api } from "../services/client";
+import { Tool } from "../types";
 
-export const getTools = async () => (await api.get('tools')).data
+export const getTools = async (): Promise<Tool[]> => (await api.get('tools')).data
