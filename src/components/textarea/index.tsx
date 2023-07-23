3005281795a8a4ca6296483d634b59ac3ad4099d
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2023-07-23 00:58:26 -0300

    Remove children from textarea component

--- src/components/textarea/index.tsx
+++ src/components/textarea/index.tsx
@@ -3,17 +3,14 @@ import './styles.css'
 interface TextareaProps {
     id: string
     label: string
-    children: string
     textareaRef?: React.Ref<HTMLTextAreaElement>
 }
 
-export function Textarea({ id, label, children, textareaRef }: TextareaProps) {
+export function Textarea({ id, label, textareaRef }: TextareaProps) {
     return (
         <fieldset className='textarea'>
             <label htmlFor={id} className='textarea__label'>{label}</label>
-            <textarea id={id} ref={textareaRef}>
-                {children}
-            </textarea>
+            <textarea id={id} ref={textareaRef} />
         </fieldset>
     )
 }
