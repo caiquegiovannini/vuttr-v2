ab581a5301638eeb10dacc0208720e7544134cce
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2023-07-30 16:11:12 -0300

    Add filter input to Actions

--- src/components/actions/index.tsx
+++ src/components/actions/index.tsx
@@ -5,6 +5,7 @@ import Add from '../../assets/add.svg'
 import { NewToolModal } from './new-tool-modal'
 
 import './styles.css'
+import { Input } from '../input'
 
 export function Actions() {
     const [isOpen, setIsOpen] = useState(false)
@@ -16,6 +17,9 @@ export function Actions() {
     return (
         <Dialog.Root open={isOpen} onOpenChange={toggleOpenModal}>
             <aside className='actions-container'>
+                <div className='actions-container__filter'>
+                    <Input id='filter' placeholder='filter by title' aria-label='tool filter' />
+                </div>
                 <Dialog.Trigger asChild>
                     <button
                         className='add-button'
