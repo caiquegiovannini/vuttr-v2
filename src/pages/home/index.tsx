8df58d280ca9a6b05057225a3711a78503e7b71e
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2023-08-17 00:00:32 -0300

    Remove tools fetch from Home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,3 @@
-import { useContext, useEffect } from 'react'
-import { ToolsContext } from '../../contexts/tools-context'
 import { Header } from '../../components/header'
 import { Actions } from '../../components/actions'
 import { ToolsList } from '../../components/tools-list'
@@ -7,12 +5,6 @@ import { ToolsList } from '../../components/tools-list'
 import './styles.css'
 
 export function Home() {
-    const { fetchTools } = useContext(ToolsContext)
-
-    useEffect(() => {
-        fetchTools()
-    }, [fetchTools])
-
     return (
         <section className='home-container'>
             <Header />
