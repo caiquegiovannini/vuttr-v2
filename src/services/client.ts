fd23a9f3792956fd87bba06b4eafca87f90d3fe7
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2024-04-12 23:18:46 -0300

    Change baseURL depending on env mode

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+console.log(import.meta.env)
+
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: import.meta.env.MODE === 'development' ?
+        'http://localhost:3000/api/' :
+        '/api/',
     headers: {
         'Content-Type': 'application/json',
     },
