eff22fb29875565bb52b47b9dd398625573ac47d
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2024-04-18 00:06:43 -0300

    Change baseurl

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: import.meta.env.BASE_URL,
+    baseURL: import.meta.env.BASE_URL || 'http://localhost:3000/api/',
     headers: {
         'Content-Type': 'application/json',
     },
