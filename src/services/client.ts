2aa2d1333670ff312238712631d40c085b61ce5c
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2024-04-17 23:06:54 -0300

    Use import.meta

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,10 +1,7 @@
 import axios from 'axios'
-import dotenv from 'dotenv'
-
-dotenv.config()
 
 export const api = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: import.meta.env.BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
