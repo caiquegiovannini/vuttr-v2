52c6b4a45606ce24badaa98c5ebd2c9a4b9d23e8
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2024-04-20 16:04:50 -0300

    Add api to getways calls

--- src/api/tools.ts
+++ src/api/tools.ts
@@ -2,6 +2,6 @@ import { api } from '../services/client'
 import { Tool } from '../types'
 import { ToolPayload } from './types'
 
-export const getTools = async (): Promise<Tool[]> => (await api.get('tools')).data
-export const addTool = async (payload: ToolPayload): Promise<{data: Tool}> => await api.post('tools', payload)
-export const removeTool = async (id: string): Promise<void> => await api.delete(`tools/${id}`)
+export const getTools = async (): Promise<Tool[]> => (await api.get('api/tools')).data
+export const addTool = async (payload: ToolPayload): Promise<{data: Tool}> => await api.post('api/tools', payload)
+export const removeTool = async (id: string): Promise<void> => await api.delete(`api/tools/${id}`)
