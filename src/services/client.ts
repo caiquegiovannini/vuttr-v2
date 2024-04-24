4fedf52f11c7e62874470f7d172c7d011fbbeb02
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2024-04-24 09:22:20 -0300

    Add access control header to allow requests

--- src/services/client.ts
+++ src/services/client.ts
@@ -4,5 +4,6 @@ export const api = axios.create({
     baseURL: 'https://vuttr-api-murex.vercel.app/api/',
     headers: {
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
     },
 })
